767e0d1fd49656b5652d39a55dd29e1aa84d5e3c
Tommy <TommyCohn116@gmail.com>
2016-06-16 16:17:37 -0400

    Webpage receives data and stuff.

--- Webpage/script.js
+++ Webpage/script.js
@@ -1,22 +1,4 @@
-//Create a WebSocket object, on localhost (127.0.0.1) port 12345
-//The ws:// is just like http:// or file://
-var ws = new WebSocket("ws://127.0.0.1:12345/");
-
-//Define a function to be executed when a message is received
-ws.onmessage = function (event) {
-    var message = formatMessage(event.data); //Store the message data to a variable
-    //document.body.innerHTML = message + "<br>" + document.body.innerHTML; //Add it to the top of the webpage
-};
-
-function sendDataRequest() {
-	ws.send("ready");
-	requestAnimationFrame(sendDataRequest);
-}
-
-ws.onopen = function() {
-	requestAnimationFrame(sendDataRequest); //This is to help keep the browser from getting lost and hanging
-}
-
+/*
 //This splits the message into an array of usable pieces, and gets rid of all the whitespace
 function formatMessage(raw) {
 	var refined = raw.split("\n");
@@ -24,5 +6,58 @@ function formatMessage(raw) {
 		refined[i] = refined[i].replace(/\s/g, ""); //Remove every space in each array item by replacing it with nothing
 	}
 	console.log(refined);
+	return refined;
+}*/
+
+var ws;
+
+function setup() {
+	ws = new WebSocket("ws://127.0.0.1:12345/");
+	ws.onmessage = function(event) {
+		//console.log(event.data);
+		mainLoop(event.data);
+	}
+	ws.onopen = function() {
+		console.log("Connection opened.");
+		sendDataRequest();
+	}
+}
+function mainLoop(data) {
+	formatted = formatRawMessage(data);
+	//console.log(formatted);
+	if(formatted.length == 46) {
+		//Get the position and quaternion from the formatted data array
+		position = [formatted[10], formatted[11], formatted[12]];
+		quaternion = [formatted[14], formatted[15], formatted[16], formatted[17]];
+		
+		//console.log(position);
+		//console.log(quaternion);
+
+		for(var i=0; i<position.length; ++i) {
+			position[i] = Number(position[i].slice(2));
+		}
+		for(var i=0; i<quaternion.length; ++i) {
+			quaternion[i] = Number(quaternion[i].slice(2));
+		}
+
+		console.log(position);
+		console.log(quaternion);
+
+		sendDataRequest();
+	}
+	else {
+		console.log("Improper data received!");
+		console.log(data);
+	}
+}
+
+function sendDataRequest() {
+	ws.send("ready");
+}
+function formatRawMessage(raw) {
+	var refined = raw.split("\n");
+	for(var i=0; i<refined.length; ++i) {
+		refined[i] = refined[i].replace(/\s/g, ""); //Remove every space in each array item by replacing it with nothing
+	}
 	return refined;
 }
